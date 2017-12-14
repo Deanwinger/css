537c2a5dc5089a7e289f11be738c6da71ca5c905
Alan <a541203951@163.com>
2017-12-14 20:23:17 +0800

    finish v-bind

--- 18.X/js/main.js
+++ 18.X/js/main.js
@@ -1,8 +1,18 @@
-var app = new Vue({
-    el: '#app',
+// var app = new Vue({
+//     el: '#app',
+//     data: {
+//         name: 'A',
+//         age: 18,
+//         sex: null,
+//     }
+// })
+
+var background = new Vue({
+    el: '#background',
     data: {
-        name: 'A',
-        age: 18,
-        sex: null,
+        url: 'http://www.baidu.com',
+        img: 'https://dummyimage.com/100x100/ffcc00/ffffff',
+        klass: 'btn btn-default',
+        isActive: true,
     }
 })
