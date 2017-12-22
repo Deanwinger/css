13b84965502e3a2018cd77392c3a9418bed17ed3
Alan <a541203951@163.com>
2017-12-22 23:43:42 +0800

    finish 18.9

---
+++ 18.X/js/component.js
@@ -0,0 +1,19 @@
+Vue.component('alert', {
+
+})
+
+var alert_component = {
+    template: '<button @click="on_click">点赞</button>',
+    methods: {
+        on_click: function() {
+            alert('Yo.');
+        }
+    }
+}
+
+new Vue({
+     el: '#seg1',
+     components: {
+         alert: alert_component
+     }
+})
--- 18.X/js/main.js
+++ 18.X/js/main.js
@@ -51,12 +51,32 @@
 //     }
 // })
 
-var muse = new Vue({
-    el: '#muse',
-    data: {
-        from: 3,
-        dest: ['华容',],
-        sex: ['male',],
-        article: 'One year after our "talk," I discovered I had breast cancer. I was thirty-two, the mother of three beautiful young children, and scared. The cancer had metastasized to my lymph nodes and the statistics were not great for long-term survival. After my surgery, friends and loved ones visited and tried to find the right words. No one knew what to say, and many said the wrong things. Others wept, and I tried to encourage them. I clung to hope myself.'
-    }
-})
+// var muse = new Vue({
+//     el: '#muse',
+//     data: {
+//         from: 3,
+//         dest: ['华容',],
+//         sex: ['male',],
+//         article: 'One year after our "talk," I discovered I had breast cancer. I was thirty-two, the mother of three beautiful young children, and scared. The cancer had metastasized to my lymph nodes and the statistics were not great for long-term survival. After my surgery, friends and loved ones visited and tried to find the right words. No one knew what to say, and many said the wrong things. Others wept, and I tried to encourage them. I clung to hope myself.'
+//     }
+// })
+
+// var muse = new Vue({
+//     el: '#logic',
+//     data: {
+//         role: 'super_admin'
+//     }
+// })
+
+// Vue.component('alert', {
+//     template: '<button @click="on_click">点赞</button>',
+//     methods: {
+//         on_click: function() {
+//             alert('Yo.');
+//         }
+//     }
+// })
+
+// new Vue({
+//      el: '#seg1',
+// })
