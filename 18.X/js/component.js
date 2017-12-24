580e0637293f373735a071dabd60fb43cc16f4d8
Alan <a541203951@163.com>
2017-12-24 21:28:44 +0800

    finish 18.X

--- 18.X/js/component.js
+++ 18.X/js/component.js
@@ -55,23 +55,57 @@
 //     }
 // }
 
-var balance_component = {
-    template: '<div><show></show></div>'
+// var balance_component = {
+//     template: '<div><show></show></div>'
 
-}
+// }
 
-var show_component = {
-    template: '<button>显示余额</button>'
-}
+// var show_component = {
+//     template: '<button>显示余额</button>'
+// }
+
+// new Vue({
+//     el: '#app',
+//     components: {
+//         // alert: alert_component,
+//         // user: user_component,
+//         show: show_component,
+//         balance: balance_component,
+        
+        
+//     }
+// })
+
+Vue.component('balance', {
+    template:
+    `
+    <div>
+        <show @show-balance="show_balance"></show>
+        <div v-if="show">您的余额是：668</div>
+    </div>
+    `,
+    data: function(){
+        return {
+            show: false,
+        }
+    },
+    methods: {
+        show_balance: function(data){
+            this.show = true;
+            console.log('data: ', data)
+        }
+    }
+})
+
+Vue.component('show', {
+    template: '<button @click="on_click()">显示余额</button>',
+    methods: {
+        on_click(){
+            this.$emit('show-balance', {a: 1, b: 2});
+        }
+    }
+})
 
 new Vue({
     el: '#app',
-    components: {
-        // alert: alert_component,
-        // user: user_component,
-        show: show_component,
-        balance: balance_component,
-        
-        
-    }
 })
