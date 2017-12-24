9a957f1a377c16d1ef7ae11565efdef5d172bfe1
Alan <a541203951@163.com>
2017-12-24 16:34:50 +0800

    stuck at 18.12

--- 18.X/js/component.js
+++ 18.X/js/component.js
@@ -1,19 +1,77 @@
-Vue.component('alert', {
+// Vue.component('alert', {
 
-})
+// })
 
-var alert_component = {
-    template: '<button @click="on_click">点赞</button>',
-    methods: {
-        on_click: function() {
-            alert('Yo.');
-        }
-    }
+// var alert_component = {
+//     template: '<button @click="on_click">点赞</button>',
+//     methods: {
+//         on_click: function() {
+//             alert('Yo.');
+//         }
+//     }
+// }
+
+// new Vue({
+//      el: '#seg1',
+//      components: {
+//          alert: alert_component
+//      }
+// })
+
+// Vue.component('like', {
+//     template: '#like-component-tpl',
+//     data: function(){
+//         return {
+//             like_count: 10,
+//             liked: false,
+//         }
+//     },
+//     methods: {
+//         toggle_like: function(){
+//             if( !this.liked ){
+//                 this.like_count++;
+//             }else{
+//                 this.like_count--; 
+//             }
+//             this.liked = !this.liked;
+//         }
+//     },
+// })
+
+// var alert_component = {
+//     template: '<button @click="on_click">点赞</button>',
+//     props: ['msg'],
+//     methods: {
+//         on_click: function() {
+//             alert(this.msg);
+//         }
+//     }
+// }
+
+// var user_component = {
+//     template: '<a href="\'/user/\' + username">{{username}}</a>',
+//     props: ['username'],
+//     methods: {
+//     }
+// }
+
+var balance_component = {
+    template: '<div><show></show></div>'
+
+}
+
+var show_component = {
+    template: '<button>显示余额</button>'
 }
 
 new Vue({
-     el: '#seg1',
-     components: {
-         alert: alert_component
-     }
+    el: '#app',
+    components: {
+        // alert: alert_component,
+        // user: user_component,
+        show: show_component,
+        balance: balance_component,
+        
+        
+    }
 })
